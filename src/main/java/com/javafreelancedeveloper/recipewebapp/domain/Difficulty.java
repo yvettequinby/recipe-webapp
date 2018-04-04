2d491c473a09a94525e90d9745308f3199607a06
peregrinetradersdev <yvette.quinby@peregrinetraders.com>
2018-04-04 14:19:21 +0200

    initial

---
+++ src/main/java/com/javafreelancedeveloper/recipewebapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.javafreelancedeveloper.recipewebapp.domain;
+
+public enum Difficulty {
+	EASY, MODERATE, HARD
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO category(description) VALUES ('American');
+INSERT INTO category(description) VALUES ('Italian');
+INSERT INTO category(description) VALUES ('Vietnamese');
+INSERT INTO category(description) VALUES ('Chinese');
+INSERT INTO category(description) VALUES ('Vegan');
+INSERT INTO category(description) VALUES ('Vegetarian');
+INSERT INTO category(description) VALUES ('Comfort');
+INSERT INTO category(description) VALUES ('Low Cal');
+
+INSERT INTO unit_of_measure(description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure(description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure(description) VALUES ('Mill');
+INSERT INTO unit_of_measure(description) VALUES ('Ounce');
+INSERT INTO unit_of_measure(description) VALUES ('Gram');
+INSERT INTO unit_of_measure(description) VALUES ('Cup');
+INSERT INTO unit_of_measure(description) VALUES ('Pinch');
+INSERT INTO unit_of_measure(description) VALUES ('Handful');
+
